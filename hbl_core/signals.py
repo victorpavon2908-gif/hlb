7977566dadb1bbb8026be4cb6cf0a383eb1e4d5d
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-26 10:34:33 -0600

    Lock global deposit minimum at 10 USDT

--- hbl_core/signals.py
+++ hbl_core/signals.py
@@ -1,11 +1,12 @@
 import logging
 
 from django.db import transaction
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from .crypto_payments import verify_and_credit_deposit
-from .models import Deposit, PaymentMethod
+from .models import Deposit, PaymentMethod, PlatformConfig
+from .payment_policies import CRYPTO_DEPOSIT_KINDS, GLOBAL_MIN_DEPOSIT_USDT
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +21,19 @@ def _verify_safely(deposit_id):
         verify_and_credit_deposit(deposit_id)
     except Exception:
         logger.exception("No se pudo completar la validación automática del depósito %s", deposit_id)
+
+
+@receiver(pre_save, sender=PlatformConfig, dispatch_uid="hbl_lock_global_deposit_minimum")
+def lock_global_deposit_minimum(sender, instance, **kwargs):
+    """El mínimo de recarga de HBL es fijo para todos: 10 USDT."""
+    instance.minimum_deposit_usd = GLOBAL_MIN_DEPOSIT_USDT
+
+
+@receiver(pre_save, sender=PaymentMethod, dispatch_uid="hbl_lock_crypto_method_minimum")
+def lock_crypto_method_minimum(sender, instance, **kwargs):
+    """Evita que un método individual contradiga el mínimo global de 10 USDT."""
+    if instance.kind in CRYPTO_DEPOSIT_KINDS:
+        instance.min_amount = GLOBAL_MIN_DEPOSIT_USDT
 
 
 @receiver(post_save, sender=Deposit, dispatch_uid="hbl_auto_verify_crypto_deposit")
